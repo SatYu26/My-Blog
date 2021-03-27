135580849d15cd5899746a97903aba287967eab3
SatYu26 <goyalsatyam8@gmail.com>
2021-03-27 23:41:17 +0530

    Creating the app component and navigation

---
+++ blog-frontend/src/pages/homepage.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+const HomePage = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello, to my blog</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Elit at imperdiet
+        dui accumsan sit amet nulla facilisi. Massa ultricies mi quis hendrerit
+        dolor magna eget est lorem. Scelerisque purus semper eget duis at tellus
+        at urna. Arcu non odio euismod lacinia at. Vel fringilla est ullamcorper
+        eget nulla. Ut sem viverra aliquet eget. Donec et odio pellentesque diam
+        volutpat commodo sed. Sem viverra aliquet eget sit amet tellus. Risus
+        feugiat in ante metus dictum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Elit at imperdiet
+        dui accumsan sit amet nulla facilisi. Massa ultricies mi quis hendrerit
+        dolor magna eget est lorem. Scelerisque purus semper eget duis at tellus
+        at urna. Arcu non odio euismod lacinia at. Vel fringilla est ullamcorper
+        eget nulla. Ut sem viverra aliquet eget. Donec et odio pellentesque diam
+        volutpat commodo sed. Sem viverra aliquet eget sit amet tellus. Risus
+        feugiat in ante metus dictum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Elit at imperdiet
+        dui accumsan sit amet nulla facilisi. Massa ultricies mi quis hendrerit
+        dolor magna eget est lorem. Scelerisque purus semper eget duis at tellus
+        at urna. Arcu non odio euismod lacinia at. Vel fringilla est ullamcorper
+        eget nulla. Ut sem viverra aliquet eget. Donec et odio pellentesque diam
+        volutpat commodo sed. Sem viverra aliquet eget sit amet tellus. Risus
+        feugiat in ante metus dictum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Elit at imperdiet
+        dui accumsan sit amet nulla facilisi. Massa ultricies mi quis hendrerit
+        dolor magna eget est lorem. Scelerisque purus semper eget duis at tellus
+        at urna. Arcu non odio euismod lacinia at. Vel fringilla est ullamcorper
+        eget nulla. Ut sem viverra aliquet eget. Donec et odio pellentesque diam
+        volutpat commodo sed. Sem viverra aliquet eget sit amet tellus. Risus
+        feugiat in ante metus dictum.
+      </p>
+    </React.Fragment>
+  );
+};
+
+export default HomePage;
