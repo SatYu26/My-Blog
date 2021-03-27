cffbd93a24d43a0993c825e51395d127038da94e
SatYu26 <goyalsatyam8@gmail.com>
2021-03-28 03:17:39 +0530

    URL parameters with react-router

--- blog-frontend/src/pages/articlePage.js
+++ blog-frontend/src/pages/articlePage.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-const ArticlePage = () => {
+import articleContent from "./article-content";
+
+const ArticlePage = ({ match }) => {
+  const name = match.params.name;
+
+  const article = articleContent.find((article) => article.name === name);
+
+  if (!article) return <h1>Article does not exist</h1>;
+
   return (
     <React.Fragment>
-      <h1>This is an article</h1>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => {
+        return <p key={key}>{paragraph}</p>;
+      })}
     </React.Fragment>
   );
 };
