518f71c2023d77362300f4609e1127e38eb64c30
SatYu26 <goyalsatyam8@gmail.com>
2021-03-28 03:51:47 +0530

    Making the articles list modular

---
+++ blog-frontend/src/components/ArticlesList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ArticlesList = ({ articles }) => {
+  return (
+    <React.Fragment>
+      {articles.map((article, key) => {
+        return (
+          <Link
+            to={`/article/${article.name}`}
+            className="article-list-item"
+            key={key}
+          >
+            <h3>{article.title}</h3>
+            <p>{article.content[0].substring(0, 150)}...</p>
+          </Link>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+export default ArticlesList;
--- blog-frontend/src/pages/articlePage.js
+++ blog-frontend/src/pages/articlePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import articleContent from "./article-content";
+import ArticlesList from "../components/ArticlesList";
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
@@ -9,12 +10,18 @@ const ArticlePage = ({ match }) => {
 
   if (!article) return <h1>Article does not exist</h1>;
 
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+
   return (
     <React.Fragment>
       <h1>{article.title}</h1>
       {article.content.map((paragraph, key) => {
         return <p key={key}>{paragraph}</p>;
       })}
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
     </React.Fragment>
   );
 };
